d43fb6622d2da76e854847c098f22e070fd983b7
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-23 12:55:37 +0200

    Add project files.

---
+++ Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=ZBC-S-jona993p;Initial Catalog=cinema;User ID=cinema;Password=cinema1;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                try
+                {
+                    Console.WriteLine("Forbindelse oprettet");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Fejl ved læsning af data: " + ex.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
